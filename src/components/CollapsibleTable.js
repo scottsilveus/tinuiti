4ad9d5b91e5448fac1b30d71a62e528af925cc48
Scott Silveus <scottsilveus@gmail.com>
2020-12-04 09:58:03 -0600

    Clean up imports

--- src/components/CollapsibleTable.js
+++ src/components/CollapsibleTable.js
@@ -1,17 +1,13 @@
 import React from 'react'
 
-import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Collapse from '@material-ui/core/Collapse'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
-
-import Row from './Row'
 
 function CollapsibleTable({ dailyRankings, open }) {
     return (
