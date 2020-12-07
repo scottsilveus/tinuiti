bf2b84757b1d0affb9d827b8e97db977da2beea7
Scott Silveus <scottsilveus@gmail.com>
2020-12-07 11:24:21 -0600

    Add table head tests

---
+++ src/__tests__/TableHead.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import TableHead from '../components/TableHead'
+
+const headers = ['header1', 'Header2']
+
+describe('TableHead', () => {
+    beforeEach(() => {
+        render(
+            <table>
+                <TableHead headers={headers} />
+            </table>
+        )
+    })
+    test('headers are displayed', () => {
+        screen.getByRole('columnheader', { name: /header1/ })
+        screen.getByRole('columnheader', { name: /Header2/ })
+    })
+})
