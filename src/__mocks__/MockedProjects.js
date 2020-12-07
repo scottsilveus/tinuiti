a1674a7a705698fef66dba7a48f4f4153c53ca9a
Scott Silveus <scottsilveus@gmail.com>
2020-12-07 10:33:35 -0600

    Add mocked project object

---
+++ src/__mocks__/MockedProjects.js
@@ -0,0 +1,27 @@
+export const oneProduct = {
+    productId: '5f95f6674cf51d5b5197ab6f',
+    productName: 'Brainclip',
+    inBuybox: false,
+    price: '$91.22',
+    image: 'http://placehold.it/32x32',
+    keyword: 'tempor aliquip enim',
+    dailyRankings: [
+        {
+            rank: 40,
+            date: '2020-10-01',
+        },
+        {
+            rank: 48,
+            date: '2020-10-02',
+        },
+    ],
+    category: 'pariatur',
+    productDescription:
+        'Ad in exercitation minim dolore consectetur irure reprehenderit dolor in. Veniam enim minim exercitation excepteur sunt aliquip elit tempor est nostrud esse tempor. Velit sint irure culpa adipisicing quis consectetur. Sit commodo ipsum pariatur aute ipsum ad nisi. Cupidatat aliquip exercitation ut magna. Incididunt quis ex minim amet mollit magna sunt dolore cupidatat proident quis. Irure proident nulla et fugiat deserunt id dolore commodo ea aliqua laboris voluptate.\r\n',
+    reviews: [
+        {
+            rating: 3,
+            count: 903,
+        },
+    ],
+}
